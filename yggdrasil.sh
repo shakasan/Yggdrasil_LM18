c82a7fb841bc6fe34f77c377cbea5857668f3589
shakasan <shakasan@sirenacorp.be>
2016-06-11 21:07:40 +0200

    Internet Apps fixes

--- yggdrasil.sh
+++ yggdrasil.sh
@@ -514,7 +514,7 @@ msg "Installation des Apps internet"
 echo "opera-stable opera-stable/add-deb-source boolean false" | sudo debconf-set-selections
 
 # to add when available : owncloud-client skype-wrapper tribler qtox birdie (pushbullet) quiterss
-sudo apt-get install -y whatsie ring-gnome frogr dropbox-share syncthing-gtk syncthing opera-stable insync google-chrome-stable midori xchat xchat-indicator chromium-browser chromium-browser-l10n geary qupzilla dropbox filezilla hexchat mumble skype imagedownloader
+sudo apt-get install -y whatsie ring-gnome frogr dropbox syncthing-gtk syncthing opera-stable insync google-chrome-stable midori xchat-gnome xchat-gnome-indicator chromium-browser chromium-browser-l10n geary qupzilla dropbox filezilla hexchat mumble skype imagedownloader
 
 cd /tmp
 
