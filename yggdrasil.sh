2bf6f3c76cd7e137dda9b983d693b814e99a7667
Francois B <shakasan@sirenacorp.be>
2017-02-03 18:47:42 +0100

    newer version of Zeal by adding a PPA

--- yggdrasil.sh
+++ yggdrasil.sh
@@ -273,6 +273,7 @@ function addPPA () {
   runCmd "sudo add-apt-repository -y ppa:tista/adapta"; smsgn "Adding ppa:tista/adapta PPA (themes)" # corebird
   runCmd "sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder"; smsgn "Adding ppa:maarten-baert/simplescreenrecorder PPA" # simplescreenrecorder
   runCmd "sudo add-apt-repository -y ppa:dhor/myway"; smsgn "Adding ppa:dhor/myway PPA" # rawtherapee (newer version)
+  runCmd "sudo add-apt-repository -y ppa:zeal-developers/ppa"; smsgn "Adding ppa:zeal-developers/ppa PPA" # Zeal (newer version)
 
   msg "Adding Opera repository"
   wget -qO- http://deb.opera.com/archive.key | sudo apt-key add - &>> $logFile && retCode $? && smsgn "Adding Opera repository key"
